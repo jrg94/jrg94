3790c0038b85ef14b9614b26e7e9c0efa25766c0
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-09 16:31:32 -0400

    Cleaned up code

--- readme.py
+++ readme.py
@@ -1,18 +1,14 @@
-from os import read
-from snakemd import Document
 import logging
+from datetime import datetime
+
 import feedparser
+from snakemd import Document
 from snakemd.generator import InlineText, MDList
 
 logger = logging.getLogger(__name__)
 
 
 def get_recent_posts() -> list:
-    """
-    Collects current posts from The Renegade Coder. 
-
-    :return: a list of posts from the How to Python series
-    """
     url = "https://therenegadecoder.com/feed/"
     feed = feedparser.parse(url).entries
     logger.debug(f"Collected {len(feed)} posts")
@@ -22,7 +18,8 @@ def get_recent_posts() -> list:
 def generate_readme(posts: list) -> Document:
     readme = Document("README")
     readme.add_header("Welcome to My Profile!")
-    readme.add_paragraph("Below you'll find an up to date list of articles by me on The Renegade Coder.").insert_link("The Renegade Coder", "https://therenegadecoder.com")
+    readme.add_paragraph("Below you'll find an up to date list of articles by me on The Renegade Coder.") \
+        .insert_link("The Renegade Coder", "https://therenegadecoder.com")
     readme.add_element(MDList([InlineText(post.title, url=post.link) for post in posts]))
     readme.add_paragraph("Also, here are some fun links you can use to support my work.")
     readme.add_element(MDList([
@@ -33,7 +30,9 @@ def generate_readme(posts: list) -> Document:
         InlineText("YouTube", url="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
     ]))
     readme.add_horizontal_rule()
-    readme.add_paragraph("This document was automatically rendered using SnakeMD.").insert_link("SnakeMD", "https://snakemd.therenegadecoder.com")
+    now = datetime.today().strftime('%Y-%m-%d')
+    readme.add_paragraph(f"This document was automatically rendered on {now} using SnakeMD.") \
+        .insert_link("SnakeMD", "https://snakemd.therenegadecoder.com")
     return readme
 
 
