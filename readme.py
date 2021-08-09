7e0dd2557fca19872aa693fc77bedd315d5d9d53
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-09 16:26:25 -0400

    Automated README

--- readme.py
+++ readme.py
@@ -1,6 +1,8 @@
+from os import read
 from snakemd import Document
 import logging
 import feedparser
+from snakemd.generator import InlineText, MDList
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +18,27 @@ def get_recent_posts() -> list:
     logger.debug(f"Collected {len(feed)} posts")
     return feed
 
+
+def generate_readme(posts: list) -> Document:
+    readme = Document("README")
+    readme.add_header("Welcome to My Profile!")
+    readme.add_paragraph("Below you'll find an up to date list of articles by me.")
+    readme.add_element(MDList([InlineText(post.title, url=post.link) for post in posts]))
+    readme.add_paragraph("Also, here are some fun links you can use to support my work.")
+    readme.add_element(MDList([
+        InlineText("Patreon", url="https://www.patreon.com/TheRenegadeCoder"),
+        InlineText("Discord", url="https://discord.gg/Jhmtj7Z"),
+        InlineText("Mailing List", url="https://newsletter.therenegadecoder.com/"),
+        InlineText("Twitter", url="https://twitter.com/RenegadeCoder94"),
+        InlineText("YouTube", url="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
+    ]))
+    readme.add_horizontal_rule()
+    readme.add_paragraph("This document was automatically rendered using SnakeMD.")
+    return readme
+
+
 if __name__ == "__main__":
-    posts = get_recent_posts()
     logging.basicConfig(level=logging.DEBUG)
-    logger.debug(posts)
+    posts = get_recent_posts()
+    readme = generate_readme(posts)
+    readme.output_page("")
