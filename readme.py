f93904cbb333eb1e494479ba798d775d2f73d33a
Jeremy Grifski <jeremy.grifski@gmail.com>
2023-04-06 20:30:25 -0400

    Converted SnakeMD Dependency to 2.0.0b1 (#9)
    * Constrained requirements
    
    * First try at migrating to 2.0.0
    
    * Added docs to README generation
    
    * Added docs
    
    * Added autopep8 to requirements file
    
    * Restructures text a bit
    
    * Cleaned up empty space

--- readme.py
+++ readme.py
@@ -4,8 +4,7 @@ from datetime import datetime
 
 import feedparser
 import subete
-from snakemd import Document, InlineText, MDList, Paragraph
-from subete.repo import SampleProgram
+from snakemd import Document, Inline, MDList, Paragraph
 
 repo = subete.load()
 logger = logging.getLogger(__name__)
@@ -25,6 +24,12 @@ emojis = [
 
 
 def get_recent_posts() -> list:
+    """
+    Retrieves the list of posts from The Renegade Coder RSS feed,
+
+    :return: a list of posts
+    """
+    logging.debug("Retrieving recent posts from The Renegade Coder")
     url = "https://therenegadecoder.com/feed/"
     feed = feedparser.parse(url)
     logger.debug(f"Loaded feed: {feed}")
@@ -33,28 +38,53 @@ def get_recent_posts() -> list:
     return entries
 
 
-def get_code_snippet() -> SampleProgram:        
+def get_code_snippet() -> subete.SampleProgram:
+    """
+    Retrieves a random code snippet from the Sample Programs repo.
+
+    :return: a sample program object
+    """
+    logger.debug("Loading sample programs repo")
     code = repo.random_program()
     return code
 
 
-def generate_readme(posts: list, code: SampleProgram) -> Document:
-    readme = Document("README")
-    readme.add_header("Welcome to My Profile!")
+def generate_readme(posts: list, code: subete.SampleProgram) -> Document:
+    """
+    Generates the README document from a list of posts and a sample program.
+
+    :param posts: a list of posts
+    :param code: a code snippet
+    :return: a markdown document to be rendered
+    """
+    readme = Document()
+    readme.add_heading("Welcome to My Profile!")
     readme.add_paragraph(f"This week's code snippet, {code}, is brought to you by Subete and the Sample Programs repo.") \
         .insert_link("Subete", url="https://subete.jeremygrifski.com/en/latest/") \
         .insert_link("Sample Programs repo", url="https://sampleprograms.io/")
-    readme.add_code(code.code().encode("ascii", "ignore").decode("ascii").strip(), lang=code.language_name())
+    readme.add_code(
+        code.code().encode("ascii", "ignore").decode("ascii").strip(),
+        lang=code.language_name()
+    )
     readme.add_paragraph("Below you'll find an up-to-date list of articles by me on The Renegade Coder.") \
         .insert_link("The Renegade Coder", "https://therenegadecoder.com")
-    readme.add_element(MDList([Paragraph([random.choice(emojis), " ", InlineText(post.title, url=post.link)]) for post in posts]))
-    readme.add_paragraph("Also, here are some fun links you can use to support my work.")
-    readme.add_element(MDList([
-        InlineText("Patreon", url="https://www.patreon.com/TheRenegadeCoder"),
-        InlineText("Discord", url="https://discord.gg/Jhmtj7Z"),
-        InlineText("Mailing List", url="https://therenegadecoder.com/about/newsletter"),
-        InlineText("Twitter", url="https://twitter.com/RenegadeCoder94"),
-        InlineText("YouTube", url="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
+    readme.add_block(MDList([
+        Paragraph([
+            random.choice(emojis),
+            " ",
+            Inline(post.title, link=post.link)
+        ])
+        for post in posts]
+    ))
+    readme.add_paragraph(
+        "Also, here are some fun links you can use to support my work."
+    )
+    readme.add_block(MDList([
+        Inline("Patreon", link="https://www.patreon.com/TheRenegadeCoder"),
+        Inline("Discord", link="https://discord.gg/Jhmtj7Z"),
+        Inline("Mailing List", link="https://therenegadecoder.com/about/newsletter"),
+        Inline("Twitter", link="https://twitter.com/RenegadeCoder94"),
+        Inline("YouTube", link="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
     ]))
     readme.add_horizontal_rule()
     now = datetime.today().strftime('%Y-%m-%d')
@@ -68,4 +98,4 @@ if __name__ == "__main__":
     posts = get_recent_posts()
     code = get_code_snippet()
     readme = generate_readme(posts, code)
-    readme.output_page("")
+    readme.dump("README")
