25a13e180eaa814b3d47739c08598c2d3892aef0
jrg94 <jeremy.grifski@gmail.com>
2023-04-06 20:14:24 -0400

    First try at migrating to 2.0.0

--- readme.py
+++ readme.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 import feedparser
 import subete
-from snakemd import Document, InlineText, MDList, Paragraph
+from snakemd import Document, Inline, MDList, Paragraph
 from subete.repo import SampleProgram
 
 repo = subete.load()
@@ -25,6 +25,7 @@ emojis = [
 
 
 def get_recent_posts() -> list:
+    logging.debug("Retrieving recent posts from The Renegade Coder")
     url = "https://therenegadecoder.com/feed/"
     feed = feedparser.parse(url)
     logger.debug(f"Loaded feed: {feed}")
@@ -39,22 +40,22 @@ def get_code_snippet() -> SampleProgram:
 
 
 def generate_readme(posts: list, code: SampleProgram) -> Document:
-    readme = Document("README")
-    readme.add_header("Welcome to My Profile!")
+    readme = Document()
+    readme.add_heading("Welcome to My Profile!")
     readme.add_paragraph(f"This week's code snippet, {code}, is brought to you by Subete and the Sample Programs repo.") \
         .insert_link("Subete", url="https://subete.jeremygrifski.com/en/latest/") \
         .insert_link("Sample Programs repo", url="https://sampleprograms.io/")
     readme.add_code(code.code().encode("ascii", "ignore").decode("ascii").strip(), lang=code.language_name())
     readme.add_paragraph("Below you'll find an up-to-date list of articles by me on The Renegade Coder.") \
         .insert_link("The Renegade Coder", "https://therenegadecoder.com")
-    readme.add_element(MDList([Paragraph([random.choice(emojis), " ", InlineText(post.title, url=post.link)]) for post in posts]))
+    readme.add_block(MDList([Paragraph([random.choice(emojis), " ", Inline(post.title, url=post.link)]) for post in posts]))
     readme.add_paragraph("Also, here are some fun links you can use to support my work.")
-    readme.add_element(MDList([
-        InlineText("Patreon", url="https://www.patreon.com/TheRenegadeCoder"),
-        InlineText("Discord", url="https://discord.gg/Jhmtj7Z"),
-        InlineText("Mailing List", url="https://therenegadecoder.com/about/newsletter"),
-        InlineText("Twitter", url="https://twitter.com/RenegadeCoder94"),
-        InlineText("YouTube", url="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
+    readme.add_block(MDList([
+        Inline("Patreon", url="https://www.patreon.com/TheRenegadeCoder"),
+        Inline("Discord", url="https://discord.gg/Jhmtj7Z"),
+        Inline("Mailing List", url="https://therenegadecoder.com/about/newsletter"),
+        Inline("Twitter", url="https://twitter.com/RenegadeCoder94"),
+        Inline("YouTube", url="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
     ]))
     readme.add_horizontal_rule()
     now = datetime.today().strftime('%Y-%m-%d')
@@ -68,4 +69,4 @@ if __name__ == "__main__":
     posts = get_recent_posts()
     code = get_code_snippet()
     readme = generate_readme(posts, code)
-    readme.output_page("")
+    readme.dump("README")
