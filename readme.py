41aacee1e8081d49db01720f01e0ca9583d07dfd
jrg94 <jeremy.grifski@gmail.com>
2023-04-06 20:19:35 -0400

    Added docs to README generation

--- readme.py
+++ readme.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import feedparser
 import subete
 from snakemd import Document, Inline, MDList, Paragraph
-from subete.repo import SampleProgram
 
 repo = subete.load()
 logger = logging.getLogger(__name__)
@@ -25,6 +24,11 @@ emojis = [
 
 
 def get_recent_posts() -> list:
+    """
+    Retrieves the list of posts from The Renegade Coder RSS feed,
+
+    :return: a list of posts
+    """
     logging.debug("Retrieving recent posts from The Renegade Coder")
     url = "https://therenegadecoder.com/feed/"
     feed = feedparser.parse(url)
@@ -34,12 +38,18 @@ def get_recent_posts() -> list:
     return entries
 
 
-def get_code_snippet() -> SampleProgram:        
+def get_code_snippet() -> subete.SampleProgram:
+    """
+    Retrieves a random code snippet from the Sample Programs repo.
+
+    :return: a sample program object
+    """
+    logger.debug("Loading sample programs repo")        
     code = repo.random_program()
     return code
 
 
-def generate_readme(posts: list, code: SampleProgram) -> Document:
+def generate_readme(posts: list, code: subete.SampleProgram) -> Document:
     readme = Document()
     readme.add_heading("Welcome to My Profile!")
     readme.add_paragraph(f"This week's code snippet, {code}, is brought to you by Subete and the Sample Programs repo.") \
@@ -48,14 +58,14 @@ def generate_readme(posts: list, code: SampleProgram) -> Document:
     readme.add_code(code.code().encode("ascii", "ignore").decode("ascii").strip(), lang=code.language_name())
     readme.add_paragraph("Below you'll find an up-to-date list of articles by me on The Renegade Coder.") \
         .insert_link("The Renegade Coder", "https://therenegadecoder.com")
-    readme.add_block(MDList([Paragraph([random.choice(emojis), " ", Inline(post.title, url=post.link)]) for post in posts]))
+    readme.add_block(MDList([Paragraph([random.choice(emojis), " ", Inline(post.title, link=post.link)]) for post in posts]))
     readme.add_paragraph("Also, here are some fun links you can use to support my work.")
     readme.add_block(MDList([
-        Inline("Patreon", url="https://www.patreon.com/TheRenegadeCoder"),
-        Inline("Discord", url="https://discord.gg/Jhmtj7Z"),
-        Inline("Mailing List", url="https://therenegadecoder.com/about/newsletter"),
-        Inline("Twitter", url="https://twitter.com/RenegadeCoder94"),
-        Inline("YouTube", url="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
+        Inline("Patreon", link="https://www.patreon.com/TheRenegadeCoder"),
+        Inline("Discord", link="https://discord.gg/Jhmtj7Z"),
+        Inline("Mailing List", link="https://therenegadecoder.com/about/newsletter"),
+        Inline("Twitter", link="https://twitter.com/RenegadeCoder94"),
+        Inline("YouTube", link="https://www.youtube.com/channel/UCpyoVwOqYRlSAEUPEn7P9hw")
     ]))
     readme.add_horizontal_rule()
     now = datetime.today().strftime('%Y-%m-%d')
